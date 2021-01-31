44a836a88e88ec025035149765891907652b4699
Jessica Dao <xjessicadao@gmail.com>
2021-01-30 23:08:30 -0800

    user model created

--- server.js
+++ server.js
@@ -9,7 +9,17 @@ var app = express();
 // process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8080;
 
+//Models for syncing
+var db = require ("./models");
+
+// Set up Express app to handle data parsing
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+//Statis directory
+app.use(express.static("public"));
+
 // Set Handlebars as the default templating engine.
+var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
@@ -28,8 +38,10 @@ app.get("/journal", function(req, res) {
     res.render();
   });
 
+db.sequelize.sync({force: true}).then(function(){
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
+});
 });
